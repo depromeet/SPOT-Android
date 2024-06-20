61749856bef64d97af071c4f983a1bc5227b2a95
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-06-21 02:25:15 +0900

    [chore/#1] dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    extra.apply {
+        set("hiltVersion", "2.46.1")
+        set("coroutineVersion", "1.7.3")
+        set("lifecycleVersion", "2.8.2")
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.1.1" apply false
+    id("com.android.library") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
 }
