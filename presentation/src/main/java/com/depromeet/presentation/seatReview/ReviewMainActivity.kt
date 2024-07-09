e78f9c061f9b0b88a7616fd3e9c37e7f394f0256
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-07-10 02:58:09 +0900

    [feature/#7] 갤러리 photo picker 업로드 구현

--- presentation/src/main/java/com/depromeet/presentation/seatReview/ReviewMainActivity.kt
+++ presentation/src/main/java/com/depromeet/presentation/seatReview/ReviewMainActivity.kt
@@ -1,9 +1,12 @@
 package com.depromeet.presentation.seatReview
 
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.core.view.isVisible
 import com.depromeet.core.base.BaseActivity
 import com.depromeet.presentation.databinding.ActivityReviewMainBinding
-import com.depromeet.presentation.extension.setOnSingleClickListener
 import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -13,14 +16,19 @@ import java.util.Locale
 class ReviewMainActivity : BaseActivity<ActivityReviewMainBinding>({
     ActivityReviewMainBinding.inflate(it)
 }) {
+
+    private val imageViews: List<View> by lazy { listOf(binding.ivFirstImage, binding.ivSecondImage, binding.ivThirdImage) }
+    private var selectedImageUris: MutableList<String> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initUploadDialog()
         initDatePickerDialog()
+        setupFragmentResultListener()
     }
 
     private fun initUploadDialog() {
-        binding.btnAddPhoto.setOnSingleClickListener {
+        binding.btnAddPhoto.setOnClickListener {
             val uploadDialogFragment = UploadDialogFragment()
             uploadDialogFragment.show(supportFragmentManager, uploadDialogFragment.tag)
         }
@@ -30,7 +38,7 @@ class ReviewMainActivity : BaseActivity<ActivityReviewMainBinding>({
         val today = Calendar.getInstance()
         val dateFormat = SimpleDateFormat("yy.MM.dd", Locale.getDefault())
         binding.tvDate.text = dateFormat.format(today.time)
-        binding.layoutDate.setOnSingleClickListener {
+        binding.layoutDate.setOnClickListener {
             val datePickerDialogFragment = DatePickerDialogFragment().apply {
                 onDateSelected = { year, month, day ->
                     val selectedDate = Calendar.getInstance()
@@ -40,5 +48,26 @@ class ReviewMainActivity : BaseActivity<ActivityReviewMainBinding>({
             }
             datePickerDialogFragment.show(supportFragmentManager, datePickerDialogFragment.tag)
         }
+    }
+
+    private fun setupFragmentResultListener() {
+        supportFragmentManager.setFragmentResultListener("requestKey", this) { _, bundle ->
+            val newSelectedImages = bundle.getStringArrayList("selected_images")
+            newSelectedImages?.let { updateSelectedImages(it) }
+        }
+    }
+
+    private fun updateSelectedImages(newImageUris: List<String>) {
+        selectedImageUris.addAll(newImageUris)
+        binding.tvAddPhoto.isVisible = selectedImageUris.isEmpty()
+
+        for ((index, uri) in selectedImageUris.withIndex()) {
+            if (index < imageViews.size) {
+                val imageView = imageViews[index] as ImageView
+                imageView.isVisible = true
+                imageView.setImageURI(Uri.parse(uri))
+            }
+        }
+        binding.btnAddPhoto.isVisible = selectedImageUris.size < imageViews.size
     }
 }
--- presentation/src/main/java/com/depromeet/presentation/seatReview/UploadDialogFragment.kt
+++ presentation/src/main/java/com/depromeet/presentation/seatReview/UploadDialogFragment.kt
@@ -2,16 +2,38 @@ package com.depromeet.presentation.seatReview
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.setFragmentResult
 import com.depromeet.core.base.BindingBottomSheetDialog
 import com.depromeet.presentation.R
 import com.depromeet.presentation.databinding.FragmentUploadBottomSheetBinding
+import com.depromeet.presentation.extension.setOnSingleClickListener
 
 class UploadDialogFragment : BindingBottomSheetDialog<FragmentUploadBottomSheetBinding>(
     R.layout.fragment_upload_bottom_sheet,
     FragmentUploadBottomSheetBinding::inflate,
 ) {
+    private lateinit var pickMultipleMediaLauncher: ActivityResultLauncher<PickVisualMediaRequest>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        pickMultipleMediaLauncher =
+            registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(3)) { uris ->
+                val uriList = uris.map { it.toString() }
+                val bundle = Bundle().apply {
+                    putStringArrayList("selected_images", ArrayList(uriList))
+                }
+                setFragmentResult("requestKey", bundle)
+                dismiss()
+            }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.btnGallery.setOnSingleClickListener {
+            pickMultipleMediaLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
     }
 }
