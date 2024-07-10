d5e0b639c1213b743fa8155b60475b430e78c70d
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-07-10 11:56:03 +0900

    feature/#9-home-ui : 커스텀 말풍선 뷰 속성 추가
    패딩, 삼각형 크기,테두리 추가
    추후 필요 여부에 따라 속성 추가 예정

--- presentation/src/main/java/com/depromeet/presentation/home/CustomSpeechBubbleView.kt
+++ presentation/src/main/java/com/depromeet/presentation/home/CustomSpeechBubbleView.kt
@@ -12,21 +12,22 @@ import com.depromeet.presentation.R
 class CustomSpeechBubbleView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
 
     private val bubblePaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val textPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val bubblePath: Path = Path()
     private val bubbleRect: RectF = RectF()
+    private val typedArray =
+        context.obtainStyledAttributes(attrs, R.styleable.CustomSpeechBubbleView)
     private var text: String = ""
-    private val textPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private var textSize: Float = 0f
     private var textColor: Int = 0
     private var backgroundColor: Int = 0
-    private val typedArray =
-        context.obtainStyledAttributes(attrs, R.styleable.CustomSpeechBubbleView)
-    private val paddingLeft = resources.getDimension(R.dimen.speech_bubble_padding_left)
-    private val paddingRight = resources.getDimension(R.dimen.speech_bubble_padding_right)
-    private val paddingTop = resources.getDimension(R.dimen.speech_bubble_padding_top)
-    private val paddingBottom = resources.getDimension(R.dimen.speech_bubble_padding_bottom)
-    private val triangleHeight = resources.getDimension(R.dimen.speech_bubble_triangle_height)
-    private val triangleWidth = 20f
+    private var paddingLeftValue: Float = 0f
+    private var paddingRightValue: Float = 0f
+    private var paddingTopValue: Float = 0f
+    private var paddingBottomValue: Float = 0f
+    private var triangleHeight = 0f
+    private var triangleWidth = 0f
+    private var cornerRadius = 0f
 
     init {
         backgroundColor = typedArray.getColor(
@@ -42,6 +43,34 @@ class CustomSpeechBubbleView(context: Context, attrs: AttributeSet?) : View(cont
             resources.getDimension(R.dimen.speech_bubble_text_size)
         )
         text = typedArray.getString(R.styleable.CustomSpeechBubbleView_text) ?: ""
+        paddingLeftValue = typedArray.getDimension(
+            R.styleable.CustomSpeechBubbleView_paddingLeft,
+            resources.getDimension(R.dimen.speech_bubble_padding_left)
+        )
+        paddingRightValue = typedArray.getDimension(
+            R.styleable.CustomSpeechBubbleView_paddingRight,
+            resources.getDimension(R.dimen.speech_bubble_padding_right)
+        )
+        paddingTopValue = typedArray.getDimension(
+            R.styleable.CustomSpeechBubbleView_paddingTop,
+            resources.getDimension(R.dimen.speech_bubble_padding_top)
+        )
+        paddingBottomValue = typedArray.getDimension(
+            R.styleable.CustomSpeechBubbleView_paddingBottom,
+            resources.getDimension(R.dimen.speech_bubble_padding_bottom)
+        )
+        triangleHeight = typedArray.getDimension(
+            R.styleable.CustomSpeechBubbleView_triangleHeight,
+            resources.getDimension(R.dimen.speech_bubble_triangle_height)
+        )
+        triangleWidth = typedArray.getDimension(
+            R.styleable.CustomSpeechBubbleView_triangleWidth,
+            resources.getDimension(R.dimen.speech_bubble_triangle_width)
+        )
+        cornerRadius = typedArray.getDimension(
+            R.styleable.CustomSpeechBubbleView_cornerRadius,
+            resources.getDimension(R.dimen.speech_bubble_corner_radius)
+        )
         typedArray.recycle()
 
         bubblePaint.color = backgroundColor
@@ -61,8 +90,9 @@ class CustomSpeechBubbleView(context: Context, attrs: AttributeSet?) : View(cont
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
         val textWidth = textPaint.measureText(text)
-        val desiredWidth = (textWidth + paddingLeft + paddingRight).toInt()
-        val desiredHeight = (paddingTop + textSize + paddingBottom + triangleHeight).toInt()
+        val desiredWidth = (textWidth + paddingBottomValue + paddingRightValue).toInt()
+        val desiredHeight =
+            (paddingTopValue + textSize + paddingBottomValue + triangleHeight).toInt()
 
         val width = resolveSize(desiredWidth, widthMeasureSpec)
         val height = resolveSize(desiredHeight, heightMeasureSpec)
@@ -78,7 +108,6 @@ class CustomSpeechBubbleView(context: Context, attrs: AttributeSet?) : View(cont
         val height = height.toFloat()
 
         val bubbleHeight = height - triangleHeight
-        val cornerRadius = resources.getDimension(R.dimen.speech_bubble_corner_radius)
         bubbleRect.set(
             0f, 0f, width, bubbleHeight
         )
