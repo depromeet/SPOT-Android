1486170b5b836f1e69ae32642f7c45a01617795d
Jokwanhee <jonotch1@naver.com>
2024-07-10 13:19:08 +0900

    [feature/#8] 야구장 검색 진입 전 UI 구현

---
+++ presentation/src/main/java/com/depromeet/presentation/viewfinder/StadiumSelectionActivity.kt
@@ -0,0 +1,73 @@
+package com.depromeet.presentation.viewfinder
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.depromeet.core.base.BaseActivity
+import com.depromeet.designsystem.SpotSnackBar
+import com.depromeet.presentation.databinding.ActivityStadiumSelectionBinding
+import com.depromeet.presentation.extension.dpToPx
+import com.depromeet.presentation.viewfinder.adapter.GridSpacingItemDecoration
+import com.depromeet.presentation.viewfinder.adapter.StadiumSelectionAdapter
+import com.depromeet.presentation.viewfinder.sample.Stadium
+import com.depromeet.presentation.viewfinder.sample.stadiums
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class StadiumSelectionActivity : BaseActivity<ActivityStadiumSelectionBinding>({
+    ActivityStadiumSelectionBinding.inflate(it)
+}) {
+    companion object {
+        const val STADIUM_EXTRA = "stadium"
+        private const val STADIUM_GRID_SPAN_COUNT = 2
+    }
+
+    private lateinit var stadiumSelectionAdapter: StadiumSelectionAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        configureRecyclerViewAdapter()
+        onClickStadium()
+        stadiumSelectionAdapter.submitList(stadiums)
+    }
+
+    private fun configureRecyclerViewAdapter() {
+        stadiumSelectionAdapter = StadiumSelectionAdapter()
+        binding.rvStadium.apply {
+            layoutManager = GridLayoutManager(context, STADIUM_GRID_SPAN_COUNT)
+            adapter = stadiumSelectionAdapter
+            addItemDecoration(
+                GridSpacingItemDecoration(
+                    spanCount = STADIUM_GRID_SPAN_COUNT,
+                    spacing = 16.dpToPx(context)
+                )
+            )
+        }
+    }
+
+    private fun onClickStadium() {
+        stadiumSelectionAdapter.itemStadiumClickListener =
+            object : StadiumSelectionAdapter.OnItemStadiumClickListener {
+                override fun onItemStadiumClick(stadium: Stadium) {
+                    if (stadium.lock) {
+                        SpotSnackBar.make(view = binding.root, onClick = {
+                            startStadiumActivity(stadium)
+                        }).show()
+                    } else {
+                        startStadiumActivity(stadium)
+                    }
+                }
+            }
+    }
+
+    private fun startStadiumActivity(stadium: Stadium) {
+        val intent = Intent(
+            this@StadiumSelectionActivity,
+            StadiumActivity::class.java
+        ).apply {
+            putExtra(STADIUM_EXTRA, stadium)
+        }
+        startActivity(intent)
+    }
+}
