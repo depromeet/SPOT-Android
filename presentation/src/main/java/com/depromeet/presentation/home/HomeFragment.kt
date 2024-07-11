ebb53ccf3fd41c2ded14ca5039a523bbd6cdfdf4
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-07-11 16:59:14 +0900

    feature/#9-home-ui : 홈 화면(시야 기록X) 1차 UI 구현(디자인 수정본)

--- presentation/src/main/java/com/depromeet/presentation/home/HomeFragment.kt
+++ presentation/src/main/java/com/depromeet/presentation/home/HomeFragment.kt
@@ -6,34 +6,18 @@ import com.depromeet.core.base.BindingFragment
 import com.depromeet.presentation.R
 import com.depromeet.presentation.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : BindingFragment<FragmentHomeBinding>(
     layoutResId = R.layout.fragment_home,
     bindingInflater = FragmentHomeBinding::inflate
 ) {
-    private val job = Job()
-    private val uiScope = CoroutineScope(Dispatchers.Main + job)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        uiScope.launch {
-            delay(3000)
-            fadeOutView(binding.csbvHomeHelp)
-        }
-
-        binding.csbvHomeHelp.setOnClickListener {
-            fadeOutView(binding.csbvHomeHelp)
-        }
         binding.ivHomeProfile.setOnClickListener { navigateToProfileEditFragment() }
         binding.ibHomeEdit.setOnClickListener { navigateToProfileEditFragment() }
-
     }
 
     private fun fadeOutView(view: View) {
@@ -41,11 +25,6 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(
             .alpha(0f)
             .setDuration(1000)
             .withEndAction { view.visibility = View.GONE }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        job.cancel()
     }
 
 
