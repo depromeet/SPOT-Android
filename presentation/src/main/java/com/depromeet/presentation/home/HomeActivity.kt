0444ad5fd4f4c5a9b5de1460bc1c759d024d501a
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-07-11 21:35:11 +0900

    refactor/#9-home-ui : 홈,프로필수정 프래그먼트 -> 액티비티 수정

--- presentation/src/main/java/com/depromeet/presentation/home/HomeActivity.kt
+++ presentation/src/main/java/com/depromeet/presentation/home/HomeActivity.kt
@@ -1,24 +1,147 @@
 package com.depromeet.presentation.home
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
+import android.view.View
+import android.widget.ImageView
+import androidx.activity.viewModels
+import androidx.lifecycle.asLiveData
+import coil.load
 import com.depromeet.core.base.BaseActivity
-import com.depromeet.presentation.R
 import com.depromeet.presentation.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeActivity : BaseActivity<ActivityHomeBinding>({
-    ActivityHomeBinding.inflate(it)
-}) {
-    @SuppressLint("CommitTransaction")
+class HomeActivity : BaseActivity<ActivityHomeBinding>(
+    ActivityHomeBinding::inflate
+) {
+    private val viewModel: HomeViewModel by viewModels()
+    private val sightList: List<View> by lazy {
+        listOf(
+            binding.clHomeNoRecord,
+            binding.clHomeOneRecord,
+            binding.clHomeTwoRecord,
+            binding.clHomeThreeRecord
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.ivHomeProfile.setOnClickListener { navigateToProfileEditActivity() }
+        binding.ibHomeEdit.setOnClickListener { navigateToProfileEditActivity() }
+        binding.clMainSight.clipToOutline = true
 
-        if(savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fcv_home, HomeFragment())
-                .commitNow()
+        viewModel.uiState.asLiveData().observe(this) { state ->
+            updateUi(state)
+        }
+
+        viewModel.getInformation()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateUi(state: HomeUiState) {
+        with(binding) {
+            val profile = state.profile
+            val recentSight = state.recentSight
+
+            tvHomeLevel.text = "Lv.${profile.level} ${profile.title}"
+            setSpannableString(profile.nickName, profile.writeCount)
+            ivHomeCheerTeam.load(profile.cheerTeam)
+            tvHomeRecentRecordName.text = recentSight.location
+            tvHomeRecentRecordDate.text = recentSight.date
+
+
+            sightList.forEachIndexed { index, view ->
+                view.visibility =
+                    if (index == recentSight.photoList.size) View.VISIBLE else View.GONE
+            }
+
+            when (recentSight.photoList.size) {
+                0 -> {
+                    tvHomeRecentRecordDate.visibility = View.GONE
+                    tvHomeRecentRecordName.visibility = View.GONE
+                }
+
+                1 -> {
+                    ivHomeRecentOneRecord1.loadAndClip(recentSight.photoList[0])
+                    tvHomeRecentOneSection.text = recentSight.section
+                }
+
+                2 -> {
+                    ivHomeRecentTwoRecord1.loadAndClip(recentSight.photoList[0])
+                    ivHomeRecentTwoRecord2.loadAndClip(recentSight.photoList[1])
+                    tvHomeRecentTwoSection.text = recentSight.section
+                }
+
+                3 -> {
+                    ivHomeRecentThreeRecord1.loadAndClip(recentSight.photoList[0])
+                    ivHomeRecentThreeRecord2.loadAndClip(recentSight.photoList[1])
+                    ivHomeRecentThreeRecord3.loadAndClip(recentSight.photoList[2])
+                    tvHomeRecentThreeSection.text = recentSight.section
+                }
+
+                else -> {}
+            }
         }
     }
+
+    private fun ImageView.loadAndClip(imageUrl: String) {
+        this.load(imageUrl)
+        this.clipToOutline = true
+    }
+
+    @SuppressLint("CommitTransaction")
+    private fun navigateToProfileEditActivity() {
+        val intent = Intent(
+            this@HomeActivity,
+            ProfileEditActivity::class.java
+        )
+        startActivity(intent)
+    }
+
+    private fun setSpannableString(
+        nickName: String,
+        writeCount: Int,
+    ) {
+        val text = "${nickName}님, 지금까지\n시야후기를\n${writeCount}번 작성했어요!"
+        val spannableBuilder = SpannableStringBuilder(text)
+
+        val startIndexNickname = 0
+        val endIndexNickname = nickName.length
+        applyBoldAndSizeSpan(spannableBuilder, startIndexNickname, endIndexNickname)
+
+        val startIndexWriteCount = text.indexOf(writeCount.toString())
+        val endIndexWriteCount = startIndexWriteCount + writeCount.toString().length
+        applyBoldAndSizeSpan(spannableBuilder, startIndexWriteCount, endIndexWriteCount)
+
+        binding.tvHomeSightChance.text = spannableBuilder
+    }
+
+    private fun applyBoldAndSizeSpan(
+        spannableBuilder: SpannableStringBuilder,
+        startIndex: Int,
+        endIndex: Int,
+    ) {
+        spannableBuilder.setSpan(
+            StyleSpan(Typeface.BOLD),
+            startIndex,
+            endIndex,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        spannableBuilder.setSpan(
+            RelativeSizeSpan(1.3f),
+            startIndex,
+            endIndex,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+    }
+
+
 }
--- presentation/src/main/java/com/depromeet/presentation/home/ProfileEditActivity.kt
+++ presentation/src/main/java/com/depromeet/presentation/home/ProfileEditActivity.kt
@@ -1,38 +1,26 @@
 package com.depromeet.presentation.home
 
 import android.os.Bundle
-import android.view.View
-import androidx.activity.OnBackPressedCallback
-import com.depromeet.core.base.BindingFragment
+import com.depromeet.core.base.BaseActivity
 import com.depromeet.presentation.R
-import com.depromeet.presentation.databinding.FragmentProfileEditBinding
+import com.depromeet.presentation.databinding.ActivityProfileEditBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ProfileEditFragment : BindingFragment<FragmentProfileEditBinding>(
-    layoutResId = R.layout.fragment_profile_edit,
-    bindingInflater = FragmentProfileEditBinding::inflate
+class ProfileEditActivity : BaseActivity<ActivityProfileEditBinding>(
+    ActivityProfileEditBinding::inflate
 ) {
 
     private lateinit var adapter: ProfileEditTeamAdapter
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setRecyclerView()
         val testData = mutableListOf<UITeamData>()
         for (i in 1..20) {
             testData.add(UITeamData("LG 트윈스", R.drawable.ic_lg_team, false))
         }
         adapter.submitList(testData)
-
-        val callback = object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                parentFragmentManager.popBackStack()
-            }
-        }
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
     }
 
     private fun setRecyclerView() {
