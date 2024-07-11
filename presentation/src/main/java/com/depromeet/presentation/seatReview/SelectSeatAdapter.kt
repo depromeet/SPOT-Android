7244f99d678b7ceed595ef32d20cb2b47bd64ad3
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-07-11 17:20:27 +0900

    [fix/#13] GridLayout Recyclerview Lisadapter로 변경

--- presentation/src/main/java/com/depromeet/presentation/seatReview/SelectSeatAdapter.kt
+++ presentation/src/main/java/com/depromeet/presentation/seatReview/SelectSeatAdapter.kt
@@ -2,11 +2,22 @@ package com.depromeet.presentation.seatReview
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.depromeet.presentation.databinding.ItemSelectSeatBinding
 
-class SelectSeatAdapter(private val dataList: List<String>) :
-    RecyclerView.Adapter<SelectSeatAdapter.ViewHolder>() {
+class SelectSeatAdapter : ListAdapter<String, SelectSeatAdapter.ViewHolder>(DiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            ItemSelectSeatBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
 
     inner class ViewHolder(private val binding: ItemSelectSeatBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -16,17 +27,13 @@ class SelectSeatAdapter(private val dataList: List<String>) :
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding =
-            ItemSelectSeatBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
-    }
+    class DiffCallback : DiffUtil.ItemCallback<String>() {
+        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem == newItem
+        }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(dataList[position])
-    }
-
-    override fun getItemCount(): Int {
-        return dataList.size
+        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem == newItem
+        }
     }
 }
