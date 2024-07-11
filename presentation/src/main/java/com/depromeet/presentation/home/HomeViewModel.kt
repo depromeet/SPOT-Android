24d623d53fe65fb23e16f9e189b4d0dc255ffef3
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-07-11 21:18:00 +0900

    feature/#9-home-ui : 홈 화면 시야 기록 있음,없음 분기 처리 UI구현

---
+++ presentation/src/main/java/com/depromeet/presentation/home/HomeViewModel.kt
@@ -0,0 +1,35 @@
+package com.depromeet.presentation.home
+
+import android.os.Parcelable
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.depromeet.presentation.home.mockData.Profile
+import com.depromeet.presentation.home.mockData.RecentSight
+import com.depromeet.presentation.home.mockData.mockDataProfile
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.parcelize.Parcelize
+import javax.inject.Inject
+
+@Parcelize
+data class HomeUiState(
+    val profile: Profile = Profile(),
+    val recentSight: RecentSight = RecentSight(),
+) : Parcelable
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState = MutableStateFlow(HomeUiState())
+    val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
+
+    fun getInformation() {
+        mockDataProfile().onEach {
+            _uiState.value = it
+        }.launchIn(viewModelScope)
+    }
+}
