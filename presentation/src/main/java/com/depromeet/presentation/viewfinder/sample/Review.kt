78bd9d69f2b396cd5012e85f05c661d3d5803559
Jokwanhee <jonotch1@naver.com>
2024-07-12 20:26:54 +0900

    [feat/#15] 샘플데이터 추가 및 Parcelize 추가

--- presentation/src/main/java/com/depromeet/presentation/viewfinder/sample/Review.kt
+++ presentation/src/main/java/com/depromeet/presentation/viewfinder/sample/Review.kt
@@ -1,10 +1,14 @@
 package com.depromeet.presentation.viewfinder.sample
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 data class Review(
     val count: Int,
     val reviewContents: List<ReviewContent>
 )
 
+@Parcelize
 data class ReviewContent(
     val profile: String,
     val user: String,
@@ -14,4 +18,4 @@ data class ReviewContent(
     val images: List<String>,
     val content: String,
     val keyword: List<Keyword>
-)
+): Parcelable
--- presentation/src/main/java/com/depromeet/presentation/viewfinder/sample/samples.kt
+++ presentation/src/main/java/com/depromeet/presentation/viewfinder/sample/samples.kt
@@ -27,7 +27,9 @@ val stadiums = listOf(
 val pictures = listOf<String>(
     "https://picsum.photos/600/400",
     "https://picsum.photos/600/400",
-    "https://picsum.photos/600/400"
+    "https://picsum.photos/600/400",
+    "https://picsum.photos/200/300",
+    "https://picsum.photos/200/300"
 )
 
 val keywords = listOf<Keyword>(
@@ -51,18 +53,18 @@ val reviewContents = listOf<ReviewContent>(
     ),
     ReviewContent(
         "https://picsum.photos/600/400",
-        "잠실의 주인은 엘지",
-        2,
-        "207블럭 1열 12번",
+        "잠실의 주인은 기아",
+        1,
+        "117블럭 2열 34번",
         "7월 7일",
         pictures,
-        "제일 앞열이라 시야는 아주 좋았어요. 다만, 앞쪽으로 사람들이 많이 다니면 조금 신경 쓰일 수 있어요",
+        "제일 앞열이라 시야는 아주 좋았어요. 다만, 앞쪽으로 사람들이 많이 다니면 조금 신경 쓰일 수 있어요.제일 앞열이라 시야는 아주 좋았어요. 다만, 앞쪽으로 사람들이 많이 다니면 조금 신경 쓰일 수 있어요",
         keywords
     ),
     ReviewContent(
         "https://picsum.photos/600/400",
-        "잠실의 주인은 엘지",
-        2,
+        "잠실의 주인은 한화",
+        3,
         "207블럭 1열 12번",
         "7월 7일",
         pictures,
