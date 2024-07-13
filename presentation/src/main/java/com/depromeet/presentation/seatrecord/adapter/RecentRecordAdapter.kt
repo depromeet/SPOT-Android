5c09a2bb129c73da4f6c9fc6ea7acaff5563a82b
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-07-13 23:34:51 +0900

    [feature/#19] 최근 시야 기록 내역 adapter UI구현

--- presentation/src/main/java/com/depromeet/presentation/seatrecord/adapter/RecentRecordAdapter.kt
+++ presentation/src/main/java/com/depromeet/presentation/seatrecord/adapter/RecentRecordAdapter.kt
@@ -1,4 +1,47 @@
 package com.depromeet.presentation.seatrecord.adapter
 
-class RecentRecordAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.depromeet.presentation.databinding.ItemRecentRecordBinding
+import com.depromeet.presentation.extension.extractDay
+import com.depromeet.presentation.extension.getDayOfWeek
+import com.depromeet.presentation.extension.loadAndClip
+import com.depromeet.presentation.seatrecord.mockdata.ReviewMockData
+import com.depromeet.presentation.util.ItemDiffCallback
+
+class RecentRecordAdapter : ListAdapter<ReviewMockData, RecentRecordViewHolder>(
+    ItemDiffCallback(
+        onItemsTheSame = { oldItem, newItem -> oldItem.id == newItem.id },
+        onContentsTheSame = { oldItem, newItem -> oldItem == newItem }
+    )
+) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecentRecordViewHolder {
+        return RecentRecordViewHolder(
+            ItemRecentRecordBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                ), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecentRecordViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+class RecentRecordViewHolder(
+    internal val binding: ItemRecentRecordBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: ReviewMockData) {
+        with(binding) {
+            ivRecentImage.loadAndClip(item.image)
+            tvRecentDateDay.text = item.date.extractDay()
+            tvRecentDay.text = item.date.getDayOfWeek()
+            tvRecentBlockName.text = item.blockName
+            tvRecentStadiumName.text = item.stadiumName
+        }
+    }
 }
--- presentation/src/main/java/com/depromeet/presentation/seatrecord/mockdata/SeatRecordMockData.kt
+++ presentation/src/main/java/com/depromeet/presentation/seatrecord/mockdata/SeatRecordMockData.kt
@@ -1,18 +1,94 @@
 package com.depromeet.presentation.seatrecord.mockdata
 
+import android.os.Parcelable
+import com.depromeet.presentation.extension.extractMonth
+import kotlinx.parcelize.Parcelize
+
 data class MonthData(
-    val month : String, //int?
-    val isClicked : Boolean
+    val month: String, //int?
+    val isClicked: Boolean,
 )
 
+data class RecordDetailMockData(
+    val profileDetailData: ProfileDetailData,
+    val reviews: List<ReviewMockData>,
+)
+
+data class ProfileDetailData(
+    val nickName: String,
+    val profileImage: String,
+    val level: Int,
+    val titleName: String,
+    val recordCount: Int,
+)
+
+@Parcelize
+data class ReviewMockData(
+    val id: Int,
+    val date: String,
+    val image: String,
+    val stadiumName: String,
+    val blockName: String,
+    val keyword: List<String>,
+) : Parcelable
+
+data class MonthReviewData(
+    val month: String,
+    val reviews: List<ReviewMockData>,
+)
+
+fun List<ReviewMockData>.groupByMonth(): List<MonthReviewData> {
+    val groupedData = this.groupBy { it.date.extractMonth(false) }
+    return groupedData.map { (month, reviews) ->
+        MonthReviewData(month, reviews)
+    }
+}
+
+fun makeSeatRecordData(): RecordDetailMockData {
+    return RecordDetailMockData(
+        profileDetailData = ProfileDetailData(
+            nickName = "노균욱",
+            profileImage = "https://picsum.photos/600/400",
+            level = 6,
+            titleName = "전설의 직관러",
+            recordCount = 37
+        ),
+        reviews = makeRecordDetailData()
+    )
+}
+
+
+fun makeRecordDetailData(): List<ReviewMockData> {
+    val list = mutableListOf<ReviewMockData>()
+    for (i in 1..8) {
+        list.add(
+            ReviewMockData(
+                id = i,
+                date = "2024-0${i % 4 + 1}-12",
+                image = "https://picsum.photos/600/400",
+                stadiumName = "서울 잠실 야구장",
+                blockName = "1루 네이비석 304블록",
+                keyword = listOf<String>(
+                    "\uD83D\uDE4D\u200D서서 응원하는 존",
+                    "☀️ 온종일 햇빛 존",
+                    "\uD83D\uDE4D\u200D서서 응원하는 존",
+                    "☀️ 온종일 햇빛 존",
+                    "\uD83D\uDE4D\u200D서서 응원하는 존"
+                )
+            )
+        )
+    }
+    return list
+}
+
 val monthList = listOf<MonthData>(
-    MonthData("전체",true),
-    MonthData("3월",false),
-    MonthData("4월",false),
-    MonthData("4월",false),
-    MonthData("5월",false),
-    MonthData("6월",false),
-    MonthData("7월",false),
-    MonthData("8월",false),
-    MonthData("9월",false),
+    MonthData("전체", true),
+    MonthData("3월", false),
+    MonthData("4월", false),
+    MonthData("4월", false),
+    MonthData("5월", false),
+    MonthData("6월", false),
+    MonthData("7월", false),
+    MonthData("8월", false),
+    MonthData("9월", false),
 )
