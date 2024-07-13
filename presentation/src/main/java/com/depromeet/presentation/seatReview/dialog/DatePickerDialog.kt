f9db8ef864c4cb895f24c81bbcf23ab5ab7a3e2d
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-07-14 00:38:21 +0900

    [refactor/#13] Calendar util 분리

--- presentation/src/main/java/com/depromeet/presentation/seatReview/dialog/DatePickerDialog.kt
+++ presentation/src/main/java/com/depromeet/presentation/seatReview/dialog/DatePickerDialog.kt
@@ -7,27 +7,23 @@ import com.depromeet.core.base.BindingBottomSheetDialog
 import com.depromeet.presentation.R
 import com.depromeet.presentation.databinding.FragmentDatePickerBottomSheetBinding
 import com.depromeet.presentation.seatReview.ReviewViewModel
+import com.depromeet.presentation.util.CalendarUtil
 import dagger.hilt.android.AndroidEntryPoint
-import java.text.SimpleDateFormat
 import java.util.Calendar
-import java.util.Locale
 
 @AndroidEntryPoint
 class DatePickerDialog : BindingBottomSheetDialog<FragmentDatePickerBottomSheetBinding>(
     R.layout.fragment_date_picker_bottom_sheet,
-    FragmentDatePickerBottomSheetBinding::inflate
+    FragmentDatePickerBottomSheetBinding::inflate,
 ) {
     private val viewModel: ReviewViewModel by activityViewModels()
     var onDateSelected: ((year: Int, month: Int, day: Int) -> Unit)? = null
 
-    companion object {
-        private val calendarInstance: Calendar by lazy { Calendar.getInstance() }
-        private const val DATE_FORMAT = "yyyy.MM.dd"
-    }
+    private val calendarInstance: Calendar by lazy { CalendarUtil.getCurrentCalendar() }
 
-    private var selectedYear = calendarInstance.get(Calendar.YEAR)
-    private var selectedMonth = calendarInstance.get(Calendar.MONTH)
-    private var selectedDay = calendarInstance.get(Calendar.DAY_OF_MONTH)
+    private var selectedYear = CalendarUtil.getYear(calendarInstance)
+    private var selectedMonth = CalendarUtil.getMonth(calendarInstance)
+    private var selectedDay = CalendarUtil.getDay(calendarInstance)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -38,18 +34,11 @@ class DatePickerDialog : BindingBottomSheetDialog<FragmentDatePickerBottomSheetB
                 selectedYear = year
                 selectedMonth = month
                 selectedDay = day
-                val selectedDate = formatUserDate(year, month, day)
+                val selectedCalendar = CalendarUtil.getCalendar(year, month, day)
+                val selectedDate = CalendarUtil.formatCalendarDate(selectedCalendar)
                 viewModel.updateSelectedDate(selectedDate)
                 onDateSelected?.invoke(year, month, day)
             }
-        }
-    }
-
-    private fun formatUserDate(year: Int, month: Int, day: Int): String {
-        return Calendar.getInstance().apply {
-            set(year, month, day)
-        }.let { calendar ->
-            SimpleDateFormat(DATE_FORMAT, Locale.getDefault()).format(calendar.time)
         }
     }
 }
