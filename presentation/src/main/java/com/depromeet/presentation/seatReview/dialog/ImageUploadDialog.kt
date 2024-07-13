08f75e174bd9e32545fa766b8399eb7a0024705e
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-07-13 22:38:49 +0900

    [feat/#13] 카메라 권한 분기 처리

--- presentation/src/main/java/com/depromeet/presentation/seatReview/dialog/ImageUploadDialog.kt
+++ presentation/src/main/java/com/depromeet/presentation/seatReview/dialog/ImageUploadDialog.kt
@@ -1,24 +1,27 @@
 package com.depromeet.presentation.seatReview.dialog
 
+import android.Manifest
 import android.app.Activity.RESULT_OK
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.setFragmentResult
 import com.depromeet.core.base.BindingBottomSheetDialog
 import com.depromeet.presentation.R
 import com.depromeet.presentation.databinding.FragmentUploadBottomSheetBinding
 import com.depromeet.presentation.extension.setOnSingleClickListener
-import com.depromeet.presentation.seatReview.ReviewViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import java.io.ByteArrayOutputStream
 
@@ -34,7 +37,7 @@ class ImageUploadDialog : BindingBottomSheetDialog<FragmentUploadBottomSheetBind
         private const val IMAGE_TITLE = "image"
     }
 
-    private val viewModel: ReviewViewModel by activityViewModels()
+    private var PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA)
     private lateinit var selectMultipleMediaLauncher: ActivityResultLauncher<PickVisualMediaRequest>
     private lateinit var takePhotoLauncher: ActivityResultLauncher<Intent>
 
@@ -47,7 +50,28 @@ class ImageUploadDialog : BindingBottomSheetDialog<FragmentUploadBottomSheetBind
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupUploadMethod()
+
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            val permissionList = PERMISSIONS_REQUIRED.toMutableList()
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            PERMISSIONS_REQUIRED = permissionList.toTypedArray()
+        }
     }
+
+    private val activityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            var permissionGranted = true
+            permissions.entries.forEach {
+                if (it.key in PERMISSIONS_REQUIRED && !it.value) {
+                    permissionGranted = false
+                }
+            }
+            if (!permissionGranted) {
+                Toast.makeText(context, "권한 요청이 거부되었습니다.", Toast.LENGTH_LONG).show()
+            } else {
+                navigateToCamera()
+            }
+        }
 
     private fun setupActivityResultLaunchers() {
         selectMultipleMediaLauncher =
@@ -75,22 +99,35 @@ class ImageUploadDialog : BindingBottomSheetDialog<FragmentUploadBottomSheetBind
     }
 
     private fun setupUploadMethod() {
-        binding.apply {
+        with(binding) {
             layoutGallery.setOnSingleClickListener {
                 selectMultipleMediaLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
             }
             layoutTakePhoto.setOnSingleClickListener {
-                val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                takePhotoLauncher.launch(takePictureIntent)
+                if (!hasPermissions(requireContext())) {
+                    activityResultLauncher.launch(PERMISSIONS_REQUIRED)
+                } else {
+                    navigateToCamera()
+                }
             }
         }
+    }
+
+    private fun hasPermissions(context: Context) = PERMISSIONS_REQUIRED.all {
+        ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun navigateToCamera() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        takePhotoLauncher.launch(takePictureIntent)
     }
 
     private fun Bitmap.toUri(context: Context): Uri {
         val bytes = ByteArrayOutputStream().apply {
             compress(Bitmap.CompressFormat.JPEG, 100, this)
         }
-        val path = MediaStore.Images.Media.insertImage(context.contentResolver, this, IMAGE_TITLE, null)
+        val path =
+            MediaStore.Images.Media.insertImage(context.contentResolver, this, IMAGE_TITLE, null)
         return Uri.parse(path)
     }
 }
