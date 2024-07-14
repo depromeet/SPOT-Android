f4f53f63f36a88b1e2c8f3b36d0359519bed5cba
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-07-14 17:42:36 +0900

    [feat/#23] 좌석, 시야 후기 개수 모두 채워졌을 때 메인뷰  텍스트 표시

--- presentation/src/main/java/com/depromeet/presentation/seatReview/ReviewViewModel.kt
+++ presentation/src/main/java/com/depromeet/presentation/seatReview/ReviewViewModel.kt
@@ -1,5 +1,6 @@
 package com.depromeet.presentation.seatReview
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
@@ -14,14 +15,24 @@ class ReviewViewModel @Inject constructor() : ViewModel() {
     private val dateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yy.MM.dd")
     private val currentDate: String = LocalDate.now().format(dateFormatter)
 
+    // 시야 후기
+
     private val _selectedDate = MutableStateFlow(currentDate)
     val selectedDate: LiveData<String> = _selectedDate.asLiveData()
+
+    private val _reviewCount = MutableStateFlow(0)
+    val reviewCount: LiveData<Int> = _reviewCount.asLiveData()
+
+    private val _selectedReviewBtn = MutableStateFlow<List<String>>(emptyList())
+    val selectedReviewBtn: LiveData<List<String>> = _selectedReviewBtn.asLiveData()
 
     private val _detailReviewText = MutableStateFlow("")
     val detailReviewText: LiveData<String> = _detailReviewText.asLiveData()
 
-    private val _selectedReviewBtn = MutableStateFlow<List<String>>(emptyList())
-    val selectedReviewBtn: LiveData<List<String>> = _selectedReviewBtn.asLiveData()
+    // 좌석 선택
+
+    private val _selectedSeatName = MutableStateFlow("")
+    val selectedSeatName: LiveData<String> = _selectedSeatName.asLiveData()
 
     private val _selectedBlock = MutableStateFlow("")
     val selectedBlock: LiveData<String> = _selectedBlock.asLiveData()
@@ -32,27 +43,45 @@ class ReviewViewModel @Inject constructor() : ViewModel() {
     private val _selectedNumber = MutableStateFlow("")
     val selectedNumber: LiveData<String> = _selectedNumber.asLiveData()
 
+    // 날짜
     fun updateSelectedDate(date: String) {
         _selectedDate.value = date
     }
 
-    fun setDetailReviewText(text: String) {
-        _detailReviewText.value = text
-    }
+    // 시야 후기
 
     fun setSelectedButtons(buttonTexts: List<String>) {
         _selectedReviewBtn.value = buttonTexts
+        Log.d("minju", selectedReviewBtn.value.toString())
+    }
+
+    fun setReviewCount(count: Int) {
+        _reviewCount.value = count
+        Log.d("minju", reviewCount.value.toString())
+    }
+
+    fun setDetailReviewText(text: String) {
+        _detailReviewText.value = text
+        Log.d("minju", detailReviewText.value.toString())
+    }
+
+    fun setSelectedSeatName(name: String) {
+        _selectedSeatName.value = name
+        Log.d("minju", selectedSeatName.value.toString())
     }
 
     fun setSelectedBlock(block: String) {
         _selectedBlock.value = block
+        Log.d("minju", selectedBlock.value.toString())
     }
 
     fun setSelectedColumn(column: String) {
         _selectedColumn.value = column
+        Log.d("minju", selectedColumn.value.toString())
     }
 
     fun setSelectedNumber(number: String) {
         _selectedNumber.value = number
+        Log.d("minju", selectedNumber.value.toString())
     }
 }
