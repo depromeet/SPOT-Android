210d124beba31f7f3658ade506060543c7885b2b
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-07-17 01:58:59 +0900

    [feat/#27] 시야 등록 repository, repositoryImpl 추가

--- domain/src/main/java/com/depromeet/domain/repository/SeatReviewRepository.kt
+++ domain/src/main/java/com/depromeet/domain/repository/SeatReviewRepository.kt
@@ -1,9 +1,29 @@
 package com.depromeet.domain.repository
 
-import com.depromeet.domain.entity.response.ExampleListModel
+import com.depromeet.domain.entity.request.SeatReviewModel
+import com.depromeet.domain.entity.response.seatReview.SeatBlockModel
+import com.depromeet.domain.entity.response.seatReview.SeatMaxModel
+import com.depromeet.domain.entity.response.seatReview.StadiumNameModel
+import com.depromeet.domain.entity.response.seatReview.StadiumSectionModel
 
 interface SeatReviewRepository {
-    suspend fun getListExample(
-        page: Int,
-    ): Result<ExampleListModel?>
+    suspend fun getStadiumName(): Result<StadiumNameModel?>
+
+    suspend fun getStadiumSection(
+        stadiumId: Int,
+    ): Result<StadiumSectionModel?>
+
+    suspend fun getSeatBlock(
+        stadiumId: Int,
+        sectionId: Int,
+    ): Result<SeatBlockModel?>
+
+    suspend fun getSeatMax(
+        stadiumId: Int,
+        sectionId: Int,
+    ): Result<SeatMaxModel?>
+
+    suspend fun postSeatReview(
+        seatReviewInfo: SeatReviewModel,
+    ): Result<Unit>
 }
