e62f2742ae6862527104a284ba29bea9d98e4c10
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-07-16 22:31:33 +0900

    [feat/#27] 시야 등록 servie 추가

--- data/src/main/java/com/depromeet/data/model/request/RequestSeatReviewDto.kt
+++ data/src/main/java/com/depromeet/data/model/request/RequestSeatReviewDto.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class SeatReviewRequestDto(
+data class RequestSeatReviewDto(
     @SerialName("stadiumId")
     val stadiumId: Int,
     @SerialName("blockId")
--- data/src/main/java/com/depromeet/data/model/response/seatReview/ResponseStadiumSectionDto.kt
+++ data/src/main/java/com/depromeet/data/model/response/seatReview/ResponseStadiumSectionDto.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class StadiumSectionResponseDto(
+data class ResponseStadiumSectionDto(
     @SerialName("seatChart")
     val seatChart: String,
     @SerialName("sectionList")
