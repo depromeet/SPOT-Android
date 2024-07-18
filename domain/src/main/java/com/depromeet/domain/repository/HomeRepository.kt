5ab570644e24047fc2ee0bf462624b8a4e900e2d
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-07-18 22:34:18 +0900

    [feature/#29] 전체 야구 구단 리스트 조회 domain module 구현

--- domain/src/main/java/com/depromeet/domain/repository/HomeRepository.kt
+++ domain/src/main/java/com/depromeet/domain/repository/HomeRepository.kt
@@ -1,10 +1,13 @@
 package com.depromeet.domain.repository
 
 import com.depromeet.domain.entity.request.home.MySeatRecordRequest
+import com.depromeet.domain.entity.response.home.BaseballTeamResponse
 import com.depromeet.domain.entity.response.home.MySeatRecordResponse
 
 interface HomeRepository {
     suspend fun getMySeatRecord(
         mySeatRecordRequest: MySeatRecordRequest,
     ): Result<MySeatRecordResponse>
+
+    suspend fun getBaseballTeam() : Result<List<BaseballTeamResponse>>
 }
