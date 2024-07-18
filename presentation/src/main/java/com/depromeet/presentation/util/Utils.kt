e37fe7756f0da53ec917843a9dae9a28a648b442
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-07-19 00:05:19 +0900

    [feat/#33] svg 파일 coil 확장 함수 업로드

--- presentation/src/main/java/com/depromeet/presentation/util/Utils.kt
+++ presentation/src/main/java/com/depromeet/presentation/util/Utils.kt
@@ -2,6 +2,10 @@ package com.depromeet.presentation.util
 
 import android.content.Context
 import android.content.Intent
+import android.widget.ImageView
+import coil.ImageLoader
+import coil.decode.SvgDecoder
+import coil.request.ImageRequest
 
 object Utils {
     fun restartApp(context: Context, toastMsg: String?) {
@@ -13,5 +17,22 @@ object Utils {
             context.startActivity(this)
         }
         Runtime.getRuntime().exit(0)
+    }
+
+    fun ImageView.loadImageFromUrl(url: String) {
+        val imageLoader = ImageLoader.Builder(this.context)
+            .components {
+                add(SvgDecoder.Factory())
+            }
+            .build()
+
+        val request = ImageRequest.Builder(this.context)
+            .crossfade(true)
+            .crossfade(2)
+            .data(url)
+            .target(this)
+            .build()
+
+        imageLoader.enqueue(request)
     }
 }
