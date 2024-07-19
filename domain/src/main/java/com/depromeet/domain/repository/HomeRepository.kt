0572629e424a22cd0d29787cdf1b967d612cf2ad
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-07-19 14:17:17 +0900

    [feature/#29] 프로필 이미지 업로드 PresignedUrl domain 모듈 구현

--- domain/src/main/java/com/depromeet/domain/repository/HomeRepository.kt
+++ domain/src/main/java/com/depromeet/domain/repository/HomeRepository.kt
@@ -3,16 +3,22 @@ package com.depromeet.domain.repository
 import com.depromeet.domain.entity.request.home.MySeatRecordRequest
 import com.depromeet.domain.entity.response.home.BaseballTeamResponse
 import com.depromeet.domain.entity.response.home.MySeatRecordResponse
+import com.depromeet.domain.entity.response.home.PresignedUrlResponse
 
 interface HomeRepository {
     suspend fun getMySeatRecord(
         mySeatRecordRequest: MySeatRecordRequest,
     ): Result<MySeatRecordResponse>
 
-    suspend fun getBaseballTeam() : Result<List<BaseballTeamResponse>>
+    suspend fun getBaseballTeam(): Result<List<BaseballTeamResponse>>
 
     suspend fun postProfileImagePresigned(
-        presignedUrl : String,
-        memberId : Int
-    ) : Result<String>
+        fileExtension: String,
+        memberId: Int,
+    ): Result<PresignedUrlResponse>
+
+    suspend fun putProfileImage(
+        presignedUrl: String,
+        image: ByteArray,
+    ): Result<Unit>
 }
