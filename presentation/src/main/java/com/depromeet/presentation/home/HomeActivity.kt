c4bcf200c21f2a55b3370b5fdf680e285936f686
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-07-24 02:32:03 +0900

    [feature/#29] 최근 리뷰(홈 피드 조회) presentation모듈 추가 (시야 기록 추후 확인 요망)
    아직 api 안나옴

--- presentation/src/main/java/com/depromeet/presentation/home/HomeActivity.kt
+++ presentation/src/main/java/com/depromeet/presentation/home/HomeActivity.kt
@@ -7,9 +7,15 @@ import android.view.View
 import androidx.activity.viewModels
 import androidx.lifecycle.asLiveData
 import coil.load
+import coil.transform.CircleCropTransformation
 import com.depromeet.core.base.BaseActivity
+import com.depromeet.core.state.UiState
+import com.depromeet.domain.entity.response.home.ProfileResponse
+import com.depromeet.domain.entity.response.home.RecentReviewResponse
+import com.depromeet.presentation.R
 import com.depromeet.presentation.databinding.ActivityHomeBinding
 import com.depromeet.presentation.extension.loadAndClip
+import com.depromeet.presentation.extension.toast
 import com.depromeet.presentation.home.viewmodel.HomeUiState
 import com.depromeet.presentation.home.viewmodel.HomeViewModel
 import com.depromeet.presentation.util.applyBoldAndSizeSpan
@@ -45,7 +51,88 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(
             updateUi(state)
         }
 
+        viewModel.profile.asLiveData().observe(this) { state ->
+            when (state) {
+                is UiState.Success -> {
+                    updateProfile(state.data)
+                }
+
+                is UiState.Failure -> {
+                    toast("실패")
+                }
+
+                is UiState.Loading -> {}
+                is UiState.Empty -> {}
+            }
+        }
+        viewModel.recentReview.asLiveData().observe(this) { state ->
+            when (state) {
+                is UiState.Success -> {
+                    updateRecentReview(state.data)
+                }
+
+                is UiState.Failure -> {
+                    toast("실패")
+                }
+
+                is UiState.Loading -> {}
+                is UiState.Empty -> {}
+            }
+        }
+
         viewModel.getInformation()
+    }
+
+    private fun updateRecentReview(review: RecentReviewResponse) = with(binding) {
+        setSpannableString(viewModel.nickname.value, viewModel.reviewCount.value)
+        tvHomeRecentRecordName.text = review.stadiumName
+        tvHomeRecentRecordDate.text = review.date //TODO : 여기 바꿔야함 CALENDER UTIL로
+        when (review.reviewImages.size) {
+            0 -> {
+                tvHomeRecentRecordDate.visibility = View.GONE
+                tvHomeRecentRecordName.visibility = View.GONE
+            }
+
+            1 -> {
+                ivHomeRecentOneRecord1.loadAndClip(review.reviewImages[0])
+                "${review.blockName}${review.seatNumber}".also { tvHomeRecentOneSection.text = it }
+            }
+
+            2 -> {
+                ivHomeRecentTwoRecord1.loadAndClip(review.reviewImages[0])
+                ivHomeRecentTwoRecord2.loadAndClip(review.reviewImages[1])
+                "${review.blockName}${review.seatNumber}".also { tvHomeRecentOneSection.text = it }
+            }
+
+            3 -> {
+                ivHomeRecentThreeRecord1.loadAndClip(review.reviewImages[0])
+                ivHomeRecentThreeRecord2.loadAndClip(review.reviewImages[1])
+                ivHomeRecentThreeRecord3.loadAndClip(review.reviewImages[2])
+                "${review.blockName}${review.seatNumber}".also { tvHomeRecentOneSection.text = it }
+            }
+            else -> {}
+        }
+    }
+
+    private fun updateProfile(profile: ProfileResponse) = with(binding) {
+        setSpannableString(viewModel.nickname.value, viewModel.reviewCount.value)
+        ivHomeProfile.load(profile.profileImage) { transformations(CircleCropTransformation()) }
+        "Lv.${profile.level} ${profile.level.levelToTitle()}".also { tvHomeLevel.text = it }
+        ivHomeCheerTeam.load(profile.teamImage)
+
+    }
+
+    /** 임시 처리 서버에서 내려주면 다시 바꿔주고 아니면 그대로 유지*/
+    private fun Int.levelToTitle(): String {
+        return when (this) {
+            1 -> getString(R.string.one_level_to_title)
+            2 -> getString(R.string.two_level_to_title)
+            3 -> getString(R.string.three_level_to_title)
+            4 -> getString(R.string.four_level_to_title)
+            5 -> getString(R.string.five_level_to_title)
+            6 -> getString(R.string.six_level_to_title)
+            else -> getString(R.string.unknown_level_to_title)
+        }
     }
 
 
--- presentation/src/main/java/com/depromeet/presentation/home/viewmodel/HomeViewModel.kt
+++ presentation/src/main/java/com/depromeet/presentation/home/viewmodel/HomeViewModel.kt
@@ -3,14 +3,17 @@ package com.depromeet.presentation.home.viewmodel
 import android.os.Parcelable
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.depromeet.core.state.UiState
+import com.depromeet.domain.entity.response.home.ProfileResponse
+import com.depromeet.domain.entity.response.home.RecentReviewResponse
+import com.depromeet.domain.repository.HomeRepository
 import com.depromeet.presentation.home.mockdata.Profile
 import com.depromeet.presentation.home.mockdata.RecentSight
-import com.depromeet.presentation.home.mockdata.mockDataProfile
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
@@ -21,14 +24,44 @@ data class HomeUiState(
 ) : Parcelable
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val homeRepository: HomeRepository,
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow<HomeUiState>(HomeUiState())
     val uiState = _uiState.asStateFlow()
 
+    private val _profile = MutableStateFlow<UiState<ProfileResponse>>(UiState.Loading)
+    val profile = _profile.asStateFlow()
+
+    private val _recentReview = MutableStateFlow<UiState<RecentReviewResponse>>(UiState.Loading)
+    val recentReview = _recentReview.asStateFlow()
+
+    val nickname = MutableStateFlow<String>("")
+    val reviewCount = MutableStateFlow(0)
+
     fun getInformation() {
-        mockDataProfile().onEach {
-            _uiState.value = it
-        }.launchIn(viewModelScope)
+        viewModelScope.launch {
+            val profileDeferred = async {
+                homeRepository.getProfile()
+            }
+            val reviewDeferred = async {
+                homeRepository.getRecentReview()
+            }
+
+            val profileResult = profileDeferred.await()
+            profileResult.onSuccess {
+                _profile.value = UiState.Success(it)
+            }.onFailure {
+                _profile.value = UiState.Failure(it.message ?: "실패")
+            }
+
+            val reviewResult = reviewDeferred.await()
+            reviewResult.onSuccess {
+                _recentReview.value = UiState.Success(it)
+            }.onFailure {
+                _recentReview.value = UiState.Failure(it.message ?: "실패")
+            }
+        }
     }
 }
