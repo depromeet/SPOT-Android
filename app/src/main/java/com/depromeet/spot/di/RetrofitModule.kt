8ecc66e517b634804df51be7e1067217b9932730
Jokwanhee <jonotch1@naver.com>
2024-07-24 12:41:43 +0900

    [fix/#31] header 사용하지 않는 api okhttpclient 추가

--- app/src/main/java/com/depromeet/spot/di/RetrofitModule.kt
+++ app/src/main/java/com/depromeet/spot/di/RetrofitModule.kt
@@ -64,6 +64,14 @@ object RetrofitModule {
         .addInterceptor(authInterceptor)
         .addInterceptor(loggingInterceptor).build()
 
+    @WebSvg
+    @Provides
+    @Singleton
+    fun provideWebSvgOkHttpClient(
+        loggingInterceptor: Interceptor
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor).build()
+
     @Provides
     @Singleton
     fun provideRetrofit(
@@ -76,7 +84,7 @@ object RetrofitModule {
     @Provides
     @Singleton
     fun provideWebSvgRetrofit(
-        client: OkHttpClient,
+        @WebSvg client: OkHttpClient,
         factory: Factory
     ): Retrofit =
         Retrofit.Builder().baseUrl(SVG_BASE_URL).client(client).addConverterFactory(factory).build()
