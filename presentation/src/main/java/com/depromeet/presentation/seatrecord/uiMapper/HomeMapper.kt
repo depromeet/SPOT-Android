02363a9dec744f1e943f9cf923c5614b49395690
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-07-25 13:54:39 +0900

    [refactor/#29] 좌석 상세 관련 필요없는 mapper 삭제

--- presentation/src/main/java/com/depromeet/presentation/seatrecord/uiMapper/HomeMapper.kt
+++ presentation/src/main/java/com/depromeet/presentation/seatrecord/uiMapper/HomeMapper.kt
@@ -1,9 +1,7 @@
 package com.depromeet.presentation.seatrecord.uiMapper
 
-import android.os.Parcelable
 import com.depromeet.domain.entity.response.home.MySeatRecordResponse
 import com.depromeet.presentation.viewfinder.sample.Keyword
-import kotlinx.parcelize.Parcelize
 
 /**
  *  리팩토링 예정
@@ -18,35 +16,6 @@ data class MonthUiData(
     var isClicked: Boolean = false,
 )
 
-@Parcelize
-data class ReviewUiData(
-    val id: Int,
-    val stadiumId: Int,
-    val stadiumName: String,
-    val blockId: Int,
-    val blockName: String,
-    val seatId: Int,
-    val rowId: Int,
-    val seatNumber: Int,
-    val date: String,
-    val content: String,
-    val images: List<ReviewImageUiData>,
-    val keywords: List<ReviewKeywordUiData>,
-) : Parcelable {
-    @Parcelize
-    data class ReviewImageUiData(
-        val id: Int,
-        val url: String,
-    ) : Parcelable
-
-    @Parcelize
-    data class ReviewKeywordUiData(
-        val id: Int,
-        val content: String,
-        val isPositive: Boolean,
-    ) : Parcelable
-}
-
 /**
  * 우선 Ui Mapper로 임시 처리 추후 관희 flowRow 변경 사항에 따라 수정 예정
  */
@@ -56,43 +25,3 @@ fun MySeatRecordResponse.ReviewResponse.ReviewKeywordResponse.toUiKeyword() =
         type = if (isPositive) 1 else 0,
         like = 0
     )
-
-fun ReviewUiData.ReviewKeywordUiData.toUiKeyword() =
-    Keyword(
-        message = content,
-        type = if (isPositive) 1 else 0,
-        like = 0
-    )
-
-
-fun MySeatRecordResponse.ReviewResponse.toUiModel(): ReviewUiData {
-    return ReviewUiData(
-        id = id,
-        stadiumId = stadiumId,
-        stadiumName = stadiumName,
-        blockId = blockId,
-        blockName = blockName,
-        seatId = seatId,
-        rowId = rowId,
-        seatNumber = seatNumber,
-        date = date,
-        content = content,
-        images = images.map { it.toUiModel() },
-        keywords = keywords.map { it.toUiModel() }
-    )
-}
-
-fun MySeatRecordResponse.ReviewResponse.ReviewImageResponse.toUiModel(): ReviewUiData.ReviewImageUiData {
-    return ReviewUiData.ReviewImageUiData(
-        id = id,
-        url = url
-    )
-}
-
-fun MySeatRecordResponse.ReviewResponse.ReviewKeywordResponse.toUiModel(): ReviewUiData.ReviewKeywordUiData {
-    return ReviewUiData.ReviewKeywordUiData(
-        id = id,
-        content = content,
-        isPositive = isPositive
-    )
-}
