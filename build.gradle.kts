845b8d3a8c399c07cbb689ba2145574e46c4e8fa
Jokwanhee <jonotch1@naver.com>
2024-07-30 23:56:26 +0900

    [feat/#40] GA 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,10 @@ buildscript {
     }
 }
 
+plugins {
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
