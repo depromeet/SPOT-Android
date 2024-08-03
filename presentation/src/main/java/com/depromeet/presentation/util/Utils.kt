ea53bd923c152117f4417bc03254936295dc7615
Jokwanhee <jonotch1@naver.com>
2024-08-03 23:21:10 +0900

    [feat/#57] status bar 테마 및 유틸 클래스 구현

--- presentation/src/main/java/com/depromeet/presentation/util/Utils.kt
+++ presentation/src/main/java/com/depromeet/presentation/util/Utils.kt
@@ -1,10 +1,20 @@
 package com.depromeet.presentation.util
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.view.View
+import android.view.Window
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import androidx.annotation.ColorRes
+import androidx.core.view.WindowCompat
 
-object Utils {
-    fun restartApp(context: Context, toastMsg: String?) {
+class Utils(
+    private val context: Context
+) {
+    fun restartApp(toastMsg: String?) {
         val packageManager = context.packageManager
         val packageName = context.packageName
         val componentName = packageManager.getLaunchIntentForPackage(packageName)?.component
@@ -13,5 +23,40 @@ object Utils {
             context.startActivity(this)
         }
         Runtime.getRuntime().exit(0)
+    }
+
+    fun setStatusBarColor(
+        window: Window,
+        @ColorRes color: Int
+    ) {
+        window.statusBarColor = context.getColor(color)
+    }
+
+    fun isStatusBarBlackIconColor(
+        window: Window
+    ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        } else {
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+    }
+
+    fun isStatusBarWhiteIconColor(
+        window: Window
+    ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.setSystemBarsAppearance(
+                0,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        } else {
+
+            window.decorView.systemUiVisibility = 0
+        }
     }
 }
