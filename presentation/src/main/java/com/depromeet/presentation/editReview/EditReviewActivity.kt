0121509564dd2163e1afbeb0b0fec42c07b322e5
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-08-08 16:46:18 +0900

    [feat/#71] 게시물 수정 화면 추가

---
+++ presentation/src/main/java/com/depromeet/presentation/editReview/EditReviewActivity.kt
@@ -0,0 +1,11 @@
+package com.depromeet.presentation.editReview
+
+import com.depromeet.core.base.BaseActivity
+import com.depromeet.presentation.databinding.ActivityEditReviewBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class EditReviewActivity : BaseActivity<ActivityEditReviewBinding>(
+    ActivityEditReviewBinding::inflate
+) {
+}
