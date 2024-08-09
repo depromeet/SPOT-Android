9e7d764dc5d64250b0c7fff29d5ebb3c08f8a3bc
Jokwanhee <jonotch1@naver.com>
2024-08-09 14:59:35 +0900

    [fix/#78] 커스텀 툴팁 컴포넌트화

--- presentation/src/main/java/com/depromeet/presentation/viewfinder/compose/CustomTooltip.kt
+++ presentation/src/main/java/com/depromeet/presentation/viewfinder/compose/CustomTooltip.kt
@@ -4,17 +4,23 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.depromeet.designsystem.compose.ui.SpotTheme
 import com.depromeet.domain.entity.request.viewfinder.RequestBlockReviewQuery
@@ -22,36 +28,49 @@ import com.depromeet.presentation.R
 
 @Composable
 fun CustomTooltip(
+    triangleWidth: Dp = 12.dp,
+    triangleHeight: Dp = 5.dp,
     reviewFilter: RequestBlockReviewQuery,
     modifier: Modifier = Modifier
 ) {
     if (reviewFilter.seatNumberIsEmpty() && reviewFilter.rowNumberIsEmpty()) {
-        Column {
+        var tooltipWidth by remember {
+            mutableStateOf(0f)
+        }
+        Column(
+            modifier = modifier
+        ) {
             Canvas(
-                modifier = Modifier,
-                onDraw = {
-                    // 삼각형을 그릴 Path 생성
-                    val path = Path().apply {
-                        moveTo(125.dp.toPx(), 0.dp.toPx()) // 삼각형의 꼭짓점
-                        lineTo(131.dp.toPx(), 5.dp.toPx()) // 오른쪽 아래 점
-                        lineTo(119.dp.toPx(), 5.dp.toPx()) // 왼쪽 아래 점
-                        lineTo(125.dp.toPx(), 0.dp.toPx())
-                    }
-
-                    drawPath(
-                        path = path,
-                        color = Color(0xE6000000)
+                modifier = Modifier
+                    .height(triangleHeight)
+            ) {
+                val trianglePath = Path().apply {
+                    moveTo(tooltipWidth / 2, 0f) // 삼각형 꼭짓점
+                    lineTo( // 오른쪽 아래 점
+                        tooltipWidth / 2 + (triangleWidth.toPx() / 2),
+                        triangleHeight.toPx()
                     )
+                    lineTo( // 왼쪽 아래 점
+                        tooltipWidth / 2 - (triangleWidth.toPx() / 2),
+                        triangleHeight.toPx()
+                    )
+                    close()
                 }
-            )
-            Spacer(modifier = Modifier.height(5.dp))
-            Box(
-                modifier = modifier
-                    .background(
-                        color = SpotTheme.colors.transferBlack03,
-                        shape = RoundedCornerShape(46.dp)
-                    )
-                    .padding(horizontal = 16.dp, vertical = 12.dp)
+
+                drawPath(
+                    path = trianglePath,
+                    color = Color(0xE6000000)
+                )
+            }
+            Box(modifier = Modifier
+                .onGloballyPositioned { layoutCoordinates ->
+                    tooltipWidth = layoutCoordinates.size.width.toFloat()
+                }
+                .background(
+                    color = SpotTheme.colors.transferBlack03,
+                    shape = RoundedCornerShape(46.dp)
+                )
+                .padding(horizontal = 16.dp, vertical = 12.dp)
             ) {
                 Text(
                     text = stringResource(id = R.string.viewfinder_tooltip_description),
