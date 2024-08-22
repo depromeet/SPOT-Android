647f5e48bbaf92fccb99a53e8cd5ee402001ae0c
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-08-22 17:42:50 +0900

    [feat/#108] 등록 게시물 종류 선택 다이얼로그 연결

---
+++ presentation/src/main/java/com/dpm/presentation/seatreview/dialog/ReviewMethodDialog.kt
@@ -0,0 +1,41 @@
+package com.dpm.presentation.seatreview.dialog
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import com.depromeet.presentation.R
+import com.depromeet.presentation.databinding.FragmentSelectReviewMethodDialogBinding
+import com.dpm.core.base.BindingDialogFragment
+import com.dpm.presentation.extension.setOnSingleClickListener
+import com.dpm.presentation.seatreview.ReviewViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ReviewMethodDialog : BindingDialogFragment<FragmentSelectReviewMethodDialogBinding>(
+    R.layout.fragment_select_review_method_dialog,
+    FragmentSelectReviewMethodDialogBinding::inflate,
+) {
+    private val viewModel: ReviewViewModel by activityViewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.TransparentBottomSheetDialogFragment)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initEvent()
+    }
+
+    private fun initEvent() {
+        binding.clUploadSight.setOnSingleClickListener {
+            // TODO : 0.5초 -> 좌석 시야 등록 플로우 이동
+        }
+        binding.clUploadIntuition.setOnSingleClickListener {
+            // TODO : 0.5초 -> 직관 후기 등록 플로우 이동
+        }
+        binding.ivExit.setOnSingleClickListener {
+            dismiss()
+        }
+    }
+}
