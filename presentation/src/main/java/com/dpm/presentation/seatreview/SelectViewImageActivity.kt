7d02e160024f69d6b83f040d501579a83a164956
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-08-23 15:58:54 +0900

    [feat/#108] 시야 사진 추가 선택뷰 UI 구현

--- presentation/src/main/java/com/dpm/presentation/seatreview/SelectViewImageActivity.kt
+++ presentation/src/main/java/com/dpm/presentation/seatreview/SelectViewImageActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import com.depromeet.presentation.databinding.ActivitySelectViewImageBinding
 import com.dpm.core.base.BaseActivity
-import com.dpm.presentation.seatreview.adapter.SelectKeywordAdapter
-import com.dpm.presentation.seatreview.viewmodel.ReviewViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,13 +11,8 @@ class SelectViewImageActivity : BaseActivity<ActivitySelectViewImageBinding>({
     ActivitySelectViewImageBinding.inflate(it)
 }) {
     private val viewModel by viewModels<ReviewViewModel>()
-    private lateinit var adapter: SelectKeywordAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        adapter = SelectKeywordAdapter()
-        binding.rvKeywordList.adapter = adapter
-        val mockData = listOf("Keyword 1", "Keyword 2", "Keyword 3", "Keyword 4", "Keyword 5")
-        adapter.submitList(mockData)
     }
 }
