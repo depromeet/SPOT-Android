af4d464942cf7c204aeef1654522ba6128cccbac
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-08-24 16:27:51 +0900

    [feat/#108] 시야 사진 선택뷰 사진, 키워드 구현

--- presentation/src/main/java/com/dpm/presentation/seatreview/SelectViewImageActivity.kt
+++ presentation/src/main/java/com/dpm/presentation/seatreview/SelectViewImageActivity.kt
@@ -2,7 +2,11 @@ package com.dpm.presentation.seatreview
 
 import ReviewData
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.activity.viewModels
+import coil.load
+import coil.transform.RoundedCornersTransformation
 import com.depromeet.presentation.databinding.ActivitySelectViewImageBinding
 import com.dpm.core.base.BaseActivity
 import com.dpm.presentation.seatreview.adapter.SelectKeywordAdapter
@@ -14,14 +18,43 @@ class SelectViewImageActivity : BaseActivity<ActivitySelectViewImageBinding>({
     ActivitySelectViewImageBinding.inflate(it)
 }) {
     private val viewModel by viewModels<ReviewViewModel>()
-    private lateinit var adapter: SelectKeywordAdapter
-
+    private val density by lazy { resources.displayMetrics.density }
+    private val radiusPx by lazy { 8 * density }
+    private var _adapter: SelectKeywordAdapter? = null
+    private val adapter get() = requireNotNull(_adapter) { "Adapter is not initialized" }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        adapter = SelectKeywordAdapter()
+        initView()
+    }
+
+    private fun initView() {
+        _adapter = SelectKeywordAdapter()
         binding.rvKeywordList.adapter = adapter
-        val mockData = listOf("Keyword 1", "Keyword 2", "Keyword 3", "Keyword 4", "Keyword 5")
-        adapter.submitList(mockData)
         val reviewData = intent.getParcelableExtra<ReviewData>("REVIEW_DATA")
+        reviewData?.let {
+            val totalReview = it.selectedGoodReview + it.selectedBadReview
+            adapter.submitList(totalReview)
+            updateImageViews(it.preSignedUrlImages)
+        }
+    }
+
+    private fun updateImageViews(urls: List<String>) {
+        val selectImages = listOf(
+            binding.clFirstImage to binding.ivFirstImage,
+            binding.clSecondImage to binding.ivSecondImage,
+            binding.clThirdImage to binding.ivThirdImage,
+        )
+
+        selectImages.forEachIndexed { index, (view, image) ->
+            if (index < urls.size) {
+                view.visibility = VISIBLE
+                val imageUri = urls[index]
+                image.load(imageUri) {
+                    transformations(RoundedCornersTransformation(radiusPx))
+                }
+            } else {
+                view.visibility = GONE
+            }
+        }
     }
 }
