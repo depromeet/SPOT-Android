be101775d7215a73269446d332a7d5a1100dcda1
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-08-25 02:20:20 +0900

    [fix/#116] 버블 커스텀뷰 elevation -> background color 변경

--- presentation/src/main/java/com/dpm/presentation/seatreview/dialog/view/SeatShareDialog.kt
+++ presentation/src/main/java/com/dpm/presentation/seatreview/dialog/view/SeatShareDialog.kt
@@ -35,8 +35,7 @@ class SeatShareDialog : BindingBottomSheetDialog<FragmentSightShareBottomSheetBi
         super.onViewCreated(view, savedInstanceState)
         initEvent()
         initView()
-        // TODO : 균욱 오빠 버블 커스텀 뷰 머지 후 확인
-        binding.csbvSelectImage.setText("시야 사진이 1장 이상인 경우 선택해주세요")
+        binding.csbvSelectImage.setTextPart("시야 사진이 ", 1, "인 경우 선택해주세요")
     }
 
     private fun initEvent() {
