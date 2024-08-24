3daf7a06dec88ada5ae08d0e151f4f0d06d6ca52
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-08-24 16:27:51 +0900

    [feat/#108] 시야 사진 선택뷰 사진, 키워드 구현

--- presentation/src/main/java/com/dpm/presentation/seatreview/dialog/view/SeatShareDialog.kt
+++ presentation/src/main/java/com/dpm/presentation/seatreview/dialog/view/SeatShareDialog.kt
@@ -3,7 +3,6 @@ package com.dpm.presentation.seatreview.dialog.view
 import ReviewData
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
