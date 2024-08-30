f90bce28ae8fe531a106cddfce7e0e4d7df4f8f3
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-08-30 12:42:44 +0900

    버전 코드 업그레이드

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -3,6 +3,6 @@ object Constants {
     const val compileSdk = 34
     const val minSdk = 28
     const val targetSdk = 34
-    var versionCode = 6
-    var versionName = "1.0.4"
+    var versionCode = 7
+    var versionName = "1.0.5"
 }
